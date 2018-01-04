e9b55eb86f7ec406b32ca859bd1d68395831ddca
Jhonnyn7 <jhonny.parra@bitsamericas.com>
2018-01-04 17:22:45 -0500

    Update controller

--- app/Http/Controllers/Process/ProcessController.php
+++ app/Http/Controllers/Process/ProcessController.php
@@ -10,13 +10,24 @@ class ProcessController extends BaseController {
   /**
    * Process controller function.
    * @param $request Request Object.
-   * @param $type type of process
+   * @param $type type of process.
    */
   public function toProcess(Request $request, $type) {
-    
+    if(method_exists($this, $type)) {
+      return $this->$type($request);
+    }
+
+    return view('/',['title' => 'Site | Url not found']);
+  }
+
+  /**
+   * Login process
+   * @param $request Request Object.
+   */
+  public function login($request) {
     $uid = $_POST['user_id'];
     $pass = $_POST['pass'];
 
-    return print_r($_POST, 1);
+    return view('/',['title' => 'Site | Dashboard']);
   }
 }
